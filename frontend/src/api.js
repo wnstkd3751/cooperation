f2c056ef4286a3d980432b10d533abdec92dd9e8
wnstkd3751 <jun1234755@gmail.com>
2026-05-05 14:46:41 +0000

    connect mongodb

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://ideal-giggle-jj675qvvwprw2pp79-8000.app.github.dev"
+  baseURL: "https://ideal-giggle-jj675qvvwprw2pp79-8000.app.github.dev/"
 });
