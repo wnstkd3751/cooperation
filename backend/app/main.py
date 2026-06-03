c2c105153f81812dfed1b61863806e4b0c08f0f4
wnstkd3751 <jun1234755@gmail.com>
2026-06-03 14:00:43 +0000

    add front

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,9 +36,7 @@ app.openapi = custom_openapi
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ideal-giggle-jj675qvvwprw2pp79-5173.app.github.dev",
-        "https://ideal-fortnight-764xrwqwj7cp6rr-5173.app.github.dev",
-        "https://cooperation-4rzk-banx753ob-jun1234755-6882s-projects.vercel.app"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
